a1120b3579976bebffb10ad6b0dbd58b177248d3
Vitor Souza <zkcromossomu@gmail.com>
2024-04-26 16:50:10 -0300

    feat: references post on user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  has_many :posts, dependent: :destroy
+
   devise :database_authenticatable, :registerable,
          :jwt_authenticatable,
          jwt_revocation_strategy: JwtDenylist
