6b590b282c88935620bb96e067d3e6f86748dc1d
Vitor Souza <zkcromossomu@gmail.com>
2024-04-26 17:52:30 -0300

    refactor: rename members controller to sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class MembersController < ApplicationController
+class SessionsController < ApplicationController
   before_action :authenticate_user!
 
   def show; end
