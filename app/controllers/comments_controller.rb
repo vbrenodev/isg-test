1b7c642d979ab1b60d7e855f9520db449c251678
Vitor Souza <zkcromossomu@gmail.com>
2024-04-27 04:26:21 -0300

    feat: add rescue from missing params

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,14 +7,23 @@ class CommentsController < ApplicationController
 
   before_action :set_comment, only: %i[show update destroy]
 
+  rescue_from ActionController::ParameterMissing do |exception|
+    @error = {
+      message: "The required parameter #{exception.param} is missing.",
+      details: exception.message
+    }
+
+    render 'shared/error', status: :unprocessable_entity
+  end
+
   def index
-    @comments = Comments.where(comment_params[:post_id])
+    @comments = Comment.where(post_id: params[:post_id])
   end
 
   def show; end
 
   def create
-    @comment = Comments.new(comment_params)
+    @comment = Comment.new(comment_params)
 
     return error_response('Comment failed to be created.', @comment.errors.full_messages) unless @comment.save
 
@@ -44,7 +53,7 @@ class CommentsController < ApplicationController
   end
 
   def comment_params
-    params.require(:comment).permit(:name, :text, :post_id)
+    params.require(:comment).permit(:name, :text).merge(post_id: params[:post_id])
   end
 
   def comment_update_params
