bbacf8c6a08c4e73a7d3c64ff61e3007a95eef4b
Vitor Souza <zkcromossomu@gmail.com>
2024-04-27 02:07:02 -0300

    feat: authenticate user when destroy session and add handle error_response

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -3,17 +3,29 @@
 # app/controllers/users/sessions_controller.rb
 module Users
   class SessionsController < Devise::SessionsController
+    prepend_before_action :current_user?, only: %i[destroy]
+
     private
 
+    def current_user?
+      error_response('User failed to log out.') if current_user.blank?
+    end
+
     def respond_with(_resource, _opts = {})
-      @message = 'You are logged in.'
-      render :create, status: :ok
+      return log_in_success if current_user
+
+      error_response('User failed to log in.')
     end
 
     def respond_to_on_destroy
       return log_out_success unless current_user
 
-      log_out_failure
+      error_response('User failed to log out.')
+    end
+
+    def log_in_success
+      @message = 'You are logged in.'
+      render :create, status: :ok
     end
 
     def log_out_success
@@ -21,9 +33,9 @@ module Users
       render :destroy, status: :ok
     end
 
-    def log_out_failure
-      @message = 'Hmm nothing happened.'
-      render :destroy, status: :unauthorized
+    def error_response(details)
+      @error = { message: 'Hmm nothing happened.', details: }
+      render 'shared/error', status: :unauthorized
     end
   end
 end
