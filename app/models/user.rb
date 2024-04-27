7a4f28df7b8f429c791b5d9ab394f08e534cbcac
Vitor Souza <zkcromossomu@gmail.com>
2024-04-26 22:27:15 -0300

    refactor: correct offenses

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,15 +7,16 @@ class User < ApplicationRecord
          :jwt_authenticatable,
          jwt_revocation_strategy: JwtDenylist
 
-  validates :email, presence: true, uniqueness: true
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
   validates :password, length: { minimum: 6 }, on: %i[create update]
   validates :password_confirmation, length: { minimum: 6 }, on: %i[create update]
-  validates_confirmation_of :password, on: %i[create update]
+  validates :password, confirmation: { on: %i[create update] }
 
   default_scope -> { where(deleted_at: nil) }
 
   def destroy
-    update_attribute(:deleted_at, Time.zone.now)
+    update_column(deleted_at: Time.current) # rubocop:disable Rails/SkipsModelValidations
   end
 
   def deleted?
